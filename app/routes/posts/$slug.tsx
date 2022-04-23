ba8c326bdf324cd6fbfcb0b5e4106e468d32d18a
Angelo Dias <oiangelodias@gmail.com>
2022-04-23 21:47:10 +0200

    Add post

---
+++ app/routes/posts/$slug.tsx
@@ -0,0 +1,7 @@
+export default function PostSlug() {
+  return (
+    <main className="max-w-4xl mx-auto">
+      <h1 className="my-6 text-3xl text-center border-b-2">Some Post</h1>
+    </main>
+  );
+}
